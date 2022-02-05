e6c9d21753b76e9b06290507a6098d64e0dc7a89
Mukhopadhyay <praneshmukherjee7@gmail.com>
2022-02-06 04:27:50 +0530

    Adding length in data.csv

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import json
 import warnings
 import pandas as pd
 from utils import utils
+from typing import Optional, Dict
+from youtube_transcript_api import YouTubeTranscriptApi
 
 # Avoiding selenium warnings
 warnings.filterwarnings('ignore')
 
-def main() -> None:
+def main(save: Optional[bool] = True, verbose: Optional[bool] = False) -> pd.DataFrame:
     with open('metadata.json', 'r') as file:
         meta = json.load(file)
     dfs = []
@@ -21,15 +23,40 @@ def main() -> None:
     dataset.CC = dataset.CC.apply(lambda x: 1 if isinstance(x, str) else 0)
     dataset = dataset[~dataset.Id.str.contains('&pp=')]
 
-    print(dataset.head())
-    dataset.to_csv('videoes.csv', index=False)
+    if verbose:
+        print(dataset.head())
+    
+    if save:
+        dataset.to_csv('videoes.csv', index=False)
+    return dataset
 
+
+def generate_transcripts(df: pd.DataFrame):
+    # Split the channelss
+    channel_dfs: Dict[pd.DataFrame] = {}
+    for channel in df.Channel.unique():
+        channel_dfs[channel] = df[df.Channel == channel]
+    
+    dfs_with_transcripts = []
+    for cname, temp in channel_dfs.items():
+        print('Currently working: {}'.format(cname))
+        transcripts = {}
+        n = temp.shape[0]
+        for i, idx in enumerate(temp.Id.tolist(), start=1):
+            try:
+                t = YouTubeTranscriptApi.get_transcript(idx, languages=('en',))
+            except Exception as err:
+                transcripts[idx] = f'ERR: {str(err)}'
+            else:
+                transcripts[idx] = ' '.join([x['text'] for x in t])
+            print(f'{i}/{n}', end='\r', flush=True)
+        temp['Transcript'] = temp.Id.apply(lambda x: transcripts.get(x))
+        dfs_with_transcripts.append(temp)
+    data = pd.concat(dfs_with_transcripts)
+    return data[~data.Transcript.str.startswith('ERR: ')]
+
+
+# Driver code
 if __name__ == '__main__':
-    main()
-
-# After wards use 
-
-'''
-from youtube_transcript_api import YouTubeTranscriptApi
-YouTubeTranscriptApi.get_transcript(VIDEO_ID, ('en', ))
-'''
+    df = main()
+    
